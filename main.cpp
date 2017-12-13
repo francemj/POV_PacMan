b45f0233bb6b6928fbc94879587621641a0c2bec
Sammy Jackson <sam.jackson@live.ca>
2017-12-13 18:49:36 -0500

    added left and right movement and multiple movement commands simultaneously

--- main.cpp
+++ main.cpp
@@ -3,394 +3,431 @@
 #include <math.h>
 
 #ifdef __APPLE__
-#include <GLUT/glut.h>
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glu.h>
+#  include <GLUT/glut.h>
 #else
-#include <GL/glut.h>
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+#  include <GL/freeglut.h>
 #endif
 
-// angle of rotation for the camera direction
 float angle = 0.0f;
 
-// actual vector representing the camera's direction
-float lx=0.0f,lz=-1.0f, ly = 0.0f;
+// hold main camera
+float mX=0.0f;
+float mZ=-1.0f;
+float mY=0.0f;
 
-// XZ position of the camera
-float x=0.0f, z=5.0f, y = 1.75f;
+//values for left and right
+float xL;
+float xR;
+float zL;
+float zR;
 
-// the key states. These variables will be zero
-//when no key is being presses
-float deltaAngle = 0.0f;
-float deltaMove = 0;
-int xOrigin = -1;
+// hold xz camera
+float x=0.0f;
+float z=5.0f;
+float y=1.75f;
 
-// width and height of the window
-int h,w;
+// key states
+float dAngle=0.0f;
+float dMoveFB = 0;
+float dMoveLR = 0;
+float dX=-1;
 
-// variables to compute frames per second
+// width and height of window
+int winHeight;
+int winWidth;
+
+// computer framerate
 int frame;
 long time, timebase;
 char s[50];
 
-// variables to hold window identifiers
-int mainWindow, subWindow1,subWindow2,subWindow3;
-//border between subwindows
-int border = 6;
+//keyboard buttons array
+bool keys[4];
 
-void setProjection(int w1, int h1)
-{
-	float ratio;
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window of zero width).
-	ratio = 1.0f * w1 / h1;
-	// Reset the coordinate system before modifying
+int mainWin, gameWin, topWin, sideWin, scoreWin;
+int winBorder = 6;
+
+void setProjection(int width, int height) {
+	
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
-	// Set the viewport to be the entire window
-        glViewport(0, 0, w1, h1);
-
-	// Set the clipping volume
-	gluPerspective(45,ratio,0.1,1000);
+	glViewport(0, 0, width, height);
+	gluPerspective(45, (width*1.0)/height, 0.1, 1000);
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void changeSize(int w1,int h1) {
+void resize(int width, int height) {
+	if(height == 0)
+		height = 1;
 
-	if(h1 == 0)
-		h1 = 1;
+	winWidth = width;
+	winHeight = height;
 
-	// we're keeping these values cause we'll need them latter
-	w = w1;
-	h = h1;
+	glutSetWindow(gameWin);
+	glutPositionWindow(winBorder,winBorder);
+	glutReshapeWindow(2*(winWidth/3.0) - 2*winBorder,  winHeight - winBorder*3/2);
+	setProjection(2*(winWidth/3.0) - 2*winBorder, winHeight - winBorder*3/2);
 
-	// set subwindow 1 as the active window
-	glutSetWindow(subWindow1);
+	glutSetWindow(topWin);
 	// resize and reposition the sub window
-	glutPositionWindow(border,border);
-	glutReshapeWindow(w-2*border, h/2 - border*3/2);
-	setProjection(w-2*border, h/2 - border*3/2);
-
-	// set subwindow 2 as the active window
-	glutSetWindow(subWindow2);
-	// resize and reposition the sub window
-	glutPositionWindow(border,(h+border)/2);
-	glutReshapeWindow(w/2-border*3/2, h/2 - border*3/2);
-	setProjection(w/2-border*3/2,h/2 - border*3/2);
+	glutPositionWindow(2*(winWidth/3.0) + winBorder/2, winBorder);
+	glutReshapeWindow(winWidth/3-winBorder*3/2, winHeight/3 - winBorder*3/2);
+	setProjection(winWidth/3-winBorder*3/2, winHeight/3 - winBorder*3/2);
 
 	// set subwindow 3 as the active window
-	glutSetWindow(subWindow3);
+	glutSetWindow(sideWin);
 	// resize and reposition the sub window
-	glutPositionWindow((w+border)/2,(h+border)/2);
-	glutReshapeWindow(w/2-border*3/2,h/2 - border*3/2);
-	setProjection(w/2-border*3/2,h/2 - border*3/2);
+	glutPositionWindow(2*(winWidth/3.0) + winBorder/2, (winHeight/3.0) + winBorder/2);
+	glutReshapeWindow(winWidth/3-winBorder*3/2, winHeight/3 - winBorder*3/2);
+	setProjection(winWidth/3-winBorder*3/2, winHeight/3 - winBorder*3/2);
+
+	glutSetWindow(scoreWin);
+	glutPositionWindow(2*(winWidth/3.0) + winBorder/2, 2*(winHeight/3.0) + winBorder/2);
+	glutReshapeWindow(winWidth/3-winBorder*3/2, winHeight/3 - winBorder*3/2);
+	setProjection(winWidth/3-winBorder*3/2, winHeight/3 - winBorder*3/2);
+
 }
 
-void drawSnowMan() {
+void drawGhost() {
 
-	glColor3f(1.0f, 1.0f, 1.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
 
-// Draw Body
-	glTranslatef(0.0f ,0.75f, 0.0f);
+	// cylinder body using quadric object
+	glTranslatef(0.0f, 0.75f, 0.0f);
 	glRotatef(-90, 1.0, 0.0, 0.0);
-	// glutSolidSphere(0.75f,20,20);
-	GLUquadricObj *quadObj; 
-
-	quadObj = gluNewQuadric (); 
-	gluQuadricDrawStyle (quadObj, GLU_FILL); 
-	gluQuadricNormals (quadObj, GLU_SMOOTH); 
-	gluCylinder (quadObj, 0.6, 0.6, 1.2, 40, 6); 
+	GLUquadricObj *quadObj;
+	quadObj = gluNewQuadric();
+	gluQuadricDrawStyle(quadObj, GLU_FILL);
+	gluQuadricNormals(quadObj, GLU_SMOOTH);
+	gluCylinder(quadObj, 0.6, 0.6, 1.2, 40, 6);
 	glRotatef(90, 1.0, 0.0, 0.0);
 
-// Draw Head
+	// head
 	glTranslatef(0.0f, 1.2f, 0.0f);
 	glutSolidSphere(0.6f,20,20);
 
-// Draw Eyes
+	// eyes
 	glPushMatrix();
-	glColor3f(0.0f,0.0f,0.0f);
+	glColor3f(1.0f,1.0f,1.0f);
 	glTranslatef(0.1f, 0.10f, 0.6f);
 	glutSolidSphere(0.05f,10,10);
 	glTranslatef(-0.2f, 0.0f, 0.0f);
 	glutSolidSphere(0.05f,10,10);
 	glPopMatrix();
-
-// Draw Nose
-	glColor3f(1.0f, 0.5f, 0.5f);
-	glRotatef(0.0f,1.0f, 0.0f, 0.0f);
-	glutSolidCone(0.08f,0.5f,10,2);
-
 	glColor3f(1.0f, 1.0f, 1.0f);
-
 }
 
+// create a string using glut
 void renderBitmapString(
-		float x,
-		float y,
-		float z,
-		void *font,
-		char *string) {
-
+	float x,
+	float y,
+	float z,
+	void *font,
+	char *string
+	) {
 	char *c;
-	glRasterPos3f(x, y,z);
-	for (c=string; *c != '\0'; c++) {
+	glRasterPos3f(x,y,z);
+	for(c=string; *c != '\0'; c++) {
 		glutBitmapCharacter(font, *c);
 	}
 }
 
-void restorePerspectiveProjection() {
-
+void switchPerspectiveProj() {
 	glMatrixMode(GL_PROJECTION);
-	// restore previous projection matrix
 	glPopMatrix();
-
-	// get back to modelview mode
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void setOrthographicProjection() {
-
-	// switch to projection mode
+void switchOrthographicProj() {
 	glMatrixMode(GL_PROJECTION);
-
-	// save previous matrix which contains the
-	//settings for the perspective projection
 	glPushMatrix();
-
-	// reset matrix
 	glLoadIdentity();
-
-	// set a 2D orthographic projection
-	gluOrtho2D(0, w, h, 0);
-
-	// switch back to modelview mode
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0, winWidth, winHeight, 0);
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void computePos(float deltaMove) {
-
-	x += deltaMove * lx * 0.1f;
-	z += deltaMove * lz * 0.1f;
+void updatePositionFB(float movement){
+	x += movement *0.1f*mX;
+	z += movement *0.1f*mZ;
+	//printf("x: %f, z: %f, mX: %f, mY: %f\n", x, z, mX, mZ);
 }
 
-// Common Render Items for all subwindows
-void renderScene2() {
+void updatePositionLR(float movement){
+	x += movement *0.1f*xL;
+	z += movement *0.1f*zL;
+	//printf("x: %f, z: %f, mX: %f, mY: %f\n", x, z, mX, mZ);
+}
 
-// Draw ground
-
+void renderShapes() {
+	// create 100x100 grey background for ground
 	glColor3f(0.9f, 0.9f, 0.9f);
-	glBegin(GL_QUADS);
-		glVertex3f(-100.0f, 0.0f, -100.0f);
-		glVertex3f(-100.0f, 0.0f,  100.0f);
-		glVertex3f( 100.0f, 0.0f,  100.0f);
-		glVertex3f( 100.0f, 0.0f, -100.0f);
-	glEnd();
+		glBegin(GL_QUADS);
+			glVertex3f(-100.0f, 0.0f, -100.0f);
+			glVertex3f(-100.0f, 0.0f,  100.0f);
+			glVertex3f( 100.0f, 0.0f,  100.0f);
+			glVertex3f( 100.0f, 0.0f, -100.0f);
+		glEnd();
 
-// Draw 36 SnowMen
-	for(int i = -3; i < 3; i++)
-		for(int j=-3; j < 3; j++)
-		{
-			glPushMatrix();
-			glTranslatef(i*10.0f, 0.0f, j * 10.0f);
-			drawSnowMan();
-			glPopMatrix();
-		}
+	// create ghosts
+	for(int i=-3; i < 3; i++) {
+		glPushMatrix();
+		glTranslatef(i*10.0f, 0.0f, 10.0f);
+		drawGhost();
+		glPopMatrix();
+	}
 }
 
-// Display func for main window
-void renderScene() {
-	glutSetWindow(mainWindow);
+// render mainWin, gameWin, topWin, sideWin, scoreWin
+void renderMainWin() {
+	glutSetWindow(mainWin);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glutSwapBuffers();
 }
 
-// Display func for sub window 1
-void renderScenesw1() {
-
-	glutSetWindow(subWindow1);
-
+void renderGameWin() {
+	glutSetWindow(gameWin);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glLoadIdentity();
-	gluLookAt(x, y, z,
-		  x + lx,y + ly,z + lz,
-		  0.0f,1.0f,0.0f);
+	gluLookAt(x, y, z, x + mX,y + mY,z + mZ, 0.0f,1.0f,0.0f);
 
-	renderScene2();
+	// create yellow circle
+	glPushMatrix();
+	glColor3f(1.0, 1.0, 0.0);
+	glTranslatef(x,y,z);
+	glutSolidSphere(0.2, 4, 4);
+	glPopMatrix();
 
-	// display fps in the top window
- 	frame++;
+	renderShapes();
+	glutSwapBuffers();
+}
+
+void renderTopWin() {
+	glutSetWindow(topWin);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluLookAt(x, y + 15, z, x,y - 1,z, mX, 0.0f, mZ);
+
+	// create yellow circle
+	glPushMatrix();
+	glColor3f(1.0, 1.0, 0.0);
+	glTranslatef(x,y,z);
+	glutSolidSphere(0.2, 4, 4);
+	glPopMatrix();
+
+	renderShapes();
+	glutSwapBuffers();
+}
+
+void renderSideWin() {
+	glutSetWindow(sideWin);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluLookAt(x-mZ*10, y, z+mX*10, x, y, z, 0.0f, 1.0f, 0.0f);
+
+	// create yellow circle
+	glPushMatrix();
+	glColor3f(1.0, 1.0, 0.0);
+	glTranslatef(x,y,z);
+	glutSolidSphere(0.2, 4, 4);
+	glPopMatrix();
+
+	renderShapes();
+	glutSwapBuffers();
+}
+
+void renderScoreWin() {
+	glutSetWindow(scoreWin);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	frame++;
 
 	time=glutGet(GLUT_ELAPSED_TIME);
 	if (time - timebase > 1000) {
-		sprintf(s,"Lighthouse3D - FPS:%4.2f",
+		sprintf(s,"PacMan FPS:%4.2f",
 			frame*1000.0/(time-timebase));
 		timebase = time;
 		frame = 0;
 	}
 
-	setOrthographicProjection();
+	switchOrthographicProj();
 
 	glPushMatrix();
 	glLoadIdentity();
 	renderBitmapString(5,30,0,GLUT_BITMAP_HELVETICA_12,s);
 	glPopMatrix();
 
-	restorePerspectiveProjection();
+	switchPerspectiveProj();
 
-	glutSwapBuffers();
-}
-
-// Display func for sub window 2
-void renderScenesw2() {
-
-	glutSetWindow(subWindow2);
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glLoadIdentity();
-	gluLookAt(x, y+15, z,
-		  x ,y - 1,z,
-		  lx,0,lz);
-
-	// Draw red cone at the location of the main camera
+	// create yellow circle
 	glPushMatrix();
-	glColor3f(1.0,0.0,0.0);
+	glColor3f(1.0, 1.0, 0.0);
 	glTranslatef(x,y,z);
-	glRotatef(180-(angle+deltaAngle)*180.0/3.14,0.0,1.0,0.0);
-	glutSolidCone(0.2,0.8f,4,4);
+	glutSolidSphere(0.2, 4, 4);
 	glPopMatrix();
 
-	renderScene2();
-
+	renderShapes();
 	glutSwapBuffers();
 }
 
-// Display func for sub window 3
-void renderScenesw3() {
-
-	glutSetWindow(subWindow3);
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glLoadIdentity();
-	gluLookAt(x-lz*10 , y, z+lx*10,
-		  x ,y ,z ,
-		  0.0f,1.0f,0.0f);
-
-	// Draw red cone at the location of the main camera
-	glPushMatrix();
-	glColor3f(1.0,0.0,0.0);
-	glTranslatef(x,y,z);
-	glRotatef(180-(angle+deltaAngle)*180.0/3.14,0.0,1.0,0.0);
-	glutSolidCone(0.2,0.8f,4,4);
-	glPopMatrix();
-
-	renderScene2();
-
-	glutSwapBuffers();
-}
-
-// Global render func
-void renderSceneAll() {
-
-	// check for keyboard movement
-	if (deltaMove) {
-		computePos(deltaMove);
-		glutSetWindow(mainWindow);
+void setScene() {
+	if (dMoveFB || dMoveLR) {
+		updatePositionFB(dMoveFB);
+		updatePositionLR(dMoveLR);
+		glutSetWindow(mainWin);
 		glutPostRedisplay();
 	}
 
-	renderScene();
-	renderScenesw1();
-	renderScenesw2();
-	renderScenesw3();
+	renderGameWin();
+	renderTopWin();
+	renderSideWin();
+	renderScoreWin();
 }
 
-// -----------------------------------
-//             KEYBOARD
-// -----------------------------------
+void keyboardActions(){
+	if(keys[0] == true){//move forward
+		if(keys[1] == true){
+			dMoveFB = 0;
+		}
+		else{
+			dMoveFB = 0.5f;
 
-void processNormalKeys(unsigned char key, int xx, int yy) {
+			if(keys[2] == true){
+				dMoveLR = -0.5f;
+			}
+			else if(keys[3] == true){
+				dMoveLR = 0.5f;
+			}
+		}
+	}
 
-	if (key == 27) {
-		glutDestroyWindow(mainWindow);
-		exit(0);
+	else if(keys [1] == true){//move backwards
+		dMoveFB = -0.5f;
+
+		if(keys[2] == true){
+			dMoveLR = -0.5f;
+		}
+		else if(keys[3] == true){
+			dMoveLR = 0.5f;
+		}
+	}
+
+	else if(keys [2] == true){//move left
+		if(keys[3] == true){
+			dMoveLR = 0;
+		}
+		else{
+			dMoveLR = -0.5f;
+		}
+	}
+
+	else if(keys [3] == true){//move right
+		dMoveLR = 0.5f;
+	}
+
+	else{
+		dMoveFB = 0;
+		dMoveLR = 0;
 	}
 }
 
-void pressKey(int key, int xx, int yy) {
-
+void keyboard(unsigned char key, int xIn, int yIn) {
 	switch (key) {
-		case GLUT_KEY_UP : deltaMove = 0.5f; break;
-		case GLUT_KEY_DOWN : deltaMove = -0.5f; break;
-	}
-	glutSetWindow(mainWindow);
-	glutPostRedisplay();
 
+		case 27: {
+			glutDestroyWindow(mainWin);
+			exit(0);
+			break;
+		}
+
+		case 'w': keys[0] = true; break;
+		case 's': keys[1] = true; break;
+		case 'a': keys[2] = true; break;
+		case 'd': keys[3] = true; break;
+	}
+	glutSetWindow(mainWin);
+	keyboardActions();
+	glutPostRedisplay();
 }
 
-void releaseKey(int key, int x, int y) {
+void uKeyboard(unsigned char key, int xIn, int yIn) {
+	switch (key) {
+		case 'w': keys[0] = false; break;
+		case 's': keys[1] = false; break;
+		case 'a': keys[2] = false; break;
+		case 'd': keys[3] = false; break;
+	}
+	keyboardActions();
+}
 
+void dSpecialKeyboard(int key, int xIn, int yIn) {
+	switch (key) {
+		case GLUT_KEY_UP : dMoveFB = 0.5f; break;
+		case GLUT_KEY_DOWN : dMoveFB = -0.5f; break;
+	}
+	glutSetWindow(mainWin);
+	glutPostRedisplay();
+}
+
+
+void uSpecialKeyboard(int key, int xIn, int yIn) {
 	switch (key) {
 		case GLUT_KEY_UP :
-		case GLUT_KEY_DOWN : deltaMove = 0;break;
-	}
-}
-
-// -----------------------------------
-//             MOUSE
-// -----------------------------------
-
-void mouseMove(int x, int y) {
-
-	// this will only be true when the left button is down
-	if (true) {
-
-		// update deltaAngle
-		deltaAngle = (x - xOrigin) * 0.010f;
-
-		// update camera's direction
-		lx = sin(angle + deltaAngle);
-		lz = -cos(angle + deltaAngle);
-
-		glutSetWindow(mainWindow);
-		glutPostRedisplay();
+		case GLUT_KEY_DOWN : dMoveFB = 0; break;
 	}
 }
 
 void mouseButton(int button, int state, int x, int y) {
-
-	// only start motion if the left button is pressed
 	if (button == GLUT_LEFT_BUTTON) {
 
-		// when the button is released
+		// onClick UP
 		if (state == GLUT_UP) {
-			angle += deltaAngle;
-			deltaAngle = 0.0f;
-			xOrigin = -1;
+			
 		}
-		else  {// state = GLUT_DOWN
-			xOrigin = x;
+		else  { // onClick DOWN
 
 		}
 	}
 }
 
-// -----------------------------------
-//             MAIN and INIT
-// -----------------------------------
+void mouseMotion(int x, int y){
+}
+
+void mouseMove(int x, int y) {
+	dAngle = (x - dX) * 0.010f;
+	
+	mX = sin(angle + dAngle);
+	mZ = -cos(angle + dAngle);
+
+	xL = sin(1.62 + dAngle);
+	zL = -cos(1.62 + dAngle);
+
+	glutSetWindow(mainWin);
+	glutPostRedisplay();
+}
+
 
 void init() {
-
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
+	//hides cursor for game
+	glutSetCursor(GLUT_CURSOR_NONE);
+
 	// register callbacks
 	glutIgnoreKeyRepeat(1);
-	glutKeyboardFunc(processNormalKeys);
-	glutSpecialFunc(pressKey);
-	glutSpecialUpFunc(releaseKey);
+	glutKeyboardFunc(keyboard);
+	glutKeyboardUpFunc(uKeyboard);
+	glutSpecialFunc(dSpecialKeyboard);
+	glutSpecialUpFunc(uSpecialKeyboard);
 	glutMouseFunc(mouseButton);
 	glutPassiveMotionFunc(mouseMove);
 }
@@ -401,28 +438,29 @@ int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100,100);
-	glutInitWindowSize(800,800);
-	mainWindow = glutCreateWindow("PacMan");
+	glutInitWindowSize(1300,800);
+	mainWin = glutCreateWindow("PacMan 3D");
 
 	// callbacks for main window
-	glutDisplayFunc(renderSceneAll);
-	glutReshapeFunc(changeSize);
-
-	// Removing the idle function to save CPU and GPU
-	//glutIdleFunc(renderSceneAll);
+	glutDisplayFunc(setScene);
+	glutReshapeFunc(resize);
 	init();
 
 	// sub windows
-	subWindow1 = glutCreateSubWindow(mainWindow, border,border,w-2*border, h/2 - border*3/2);
-	glutDisplayFunc(renderScenesw1);
+	gameWin = glutCreateSubWindow(mainWin, winBorder,winBorder,2*(winWidth/3.0) - 2*winBorder, winHeight - winBorder*3/2);
+	glutDisplayFunc(renderGameWin);
 	init();
 
-	subWindow2 = glutCreateSubWindow(mainWindow, border,(h+border)/2,w/2-border*3/2, h/2 - border*3/2);
-	glutDisplayFunc(renderScenesw2);
+	topWin = glutCreateSubWindow(mainWin, 2*(winWidth/3.0) + winBorder/2, winBorder, winWidth/3-winBorder*3/2, winHeight/3 - winBorder*3/2);
+	glutDisplayFunc(renderTopWin);
 	init();
 
-	subWindow3 = glutCreateSubWindow(mainWindow, (w+border)/2,(h+border)/2,w/2-border*3/2,h/2 - border*3/2);
-	glutDisplayFunc(renderScenesw3);
+	sideWin = glutCreateSubWindow(mainWin, 2*(winWidth/3.0) + winBorder/2, (winHeight/3.0) + winBorder/2, winWidth/3-winBorder*3/2, winHeight/3 - winBorder*3/2);
+	glutDisplayFunc(renderSideWin);
+	init();
+
+	scoreWin = glutCreateSubWindow(mainWin, 2*(winWidth/3.0) + winBorder/2, 2*(winHeight/3.0) + winBorder/2, winWidth/3-winBorder*3/2, winHeight/3 - winBorder*3/2);
+	glutDisplayFunc(renderScoreWin);
 	init();
 
 	// enter GLUT event processing cycle
@@ -430,3 +468,4 @@ int main(int argc, char **argv) {
 	
 	return 1;
 }
+
